3ffba27c32d3bede3bbb51355f75301fa97d94e3
Crigerprogrammer <crigerprogrammer@gmail.com>
2018-11-25 12:26:51 -0600

    Polimorfismo
    Encapsulamiento

--- lib/testFile.rb
+++ lib/testFile.rb
@@ -1,21 +1,16 @@
 class Humano
-	attr_accessor :name , :age, :genero
-	def greet
-		puts "Hola, mi nombre es #{@name}"
+	attr_accessor :name, :age, :genre
+
+	def initialize(name, age, genre)
+		@name = name
+		@age = age
+		@genre = genre
 	end
+
 end
 
-cristian = Humano.new 
-cristian.name = "Cristian Hernandez"
-cristian.age = 20
-cristian.genero = "Masculino"
-
-cristian.greet
-cristian.name = "Gerardo"
-cristian.greet
-
-gerardo = Humano.new
-gerardo.name = "Cristian Hernandez"
-gerardo.age = 19
-gerardo.genero = "Masculino too"
-gerardo.greet
+#Crear Gerardo
+gerardo = Humano.new("Gerardo Hernandez", 20, "Masculino")
+puts gerardo.name
+puts gerardo.age
+puts gerardo.genre
